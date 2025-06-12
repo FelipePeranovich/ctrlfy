bb3ca5e7eed8335aef0081fb2fe84335edea7f28
FELIPE PERANOVICH <felipe.perano@gmail.com>
2025-06-11 21:11:08 -0300

    11/06/2025

--- marketplace.php
+++ marketplace.php
@@ -13,7 +13,7 @@
 <h4 class="logo">Ctrlfy</h4>
 <ul class="nav flex-column mt-4">
 <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
-<li class="nav-item"><a class="nav-link" href="#">Produtos</a></li>
+<li class="nav-item"><a class="nav-link" href="produtos.php">Produtos</a></li>
 <li class="nav-item"><a class="nav-link" href="estoque.php">Estoque</a></li>
 <li class="nav-item"><a class="nav-link active" href="#">Marketplaces</a></li>
 </ul>
